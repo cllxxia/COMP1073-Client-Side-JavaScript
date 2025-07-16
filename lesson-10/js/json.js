6b98678266a3ed197ed9a0c33204812af7162ebb
Claudia Scali <claudiascaliii@gmail.com>
2025-07-16 18:43:45 -0400

    Week 10 JSON

--- lesson-10/js/json.js
+++ lesson-10/js/json.js
@@ -1,25 +1,30 @@
-/* STEP 2: Reference the HEADER and the SECTION elements with variables */
+    /* STEP 2: Reference the HEADER and the SECTION elements with variables */
+    const header = document.querySelector('header');
+    const section = document.querySelector('section');
 
+    // STEP 3a: Create the asynchronous function populate()
+    async function populate() {
+        // Introducing JavaScript Object Notation (JSON): https://json.org/
+        // STEP 4a: Store the URL of a JSON file in a variable */
+        // STEP 4b: Store the URL of a JSON file in a variable
 
-// STEP 3a: Create the asynchronous function populate()
+        
+        // STEP 5: Use the new URL to create a new request object
+    
+        // STEP 6: Make a network request with the fetch() function, which returns a Response object
+    
+        // STEP 7: Capture the returned Response object and covert to a JSON object using json()
+    
+        // STEP 8: Output the iScream JSON object to the console 
+    
+        // STEP 9a: Invoke the populateHeader function here, then build it below
+    
+        // STEP 10a: Invoke the showTopFlavors function here, then build it below
+    
+}
 
-    // Introducing JavaScript Object Notation (JSON): https://json.org/
-    // STEP 4: Store the URL of a JSON file in a variable */
     
-    // STEP 5: Use the new URL to create a new request object
     
-    // STEP 6: Make a network request with the fetch() function, which returns a Response object
-    
-    // STEP 7: Capture the returned Response object and covert to a JSON object using json()
-    
-    // STEP 8: Output the iScream JSON object to the console 
-    
-    // STEP 9a: Invoke the populateHeader function here, then build it below
-    
-    // STEP 10a: Invoke the showTopFlavors function here, then build it below
-    
-
-
 // STEP 3b: Call the populate() function
 
 
